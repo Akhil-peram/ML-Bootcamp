4a39dd186325ac39e96f23eca9958ae664121e11
Akhil-peram <akhil01011@gmail.com>
2026-07-28 17:29:31 +0530

    added module

---
+++ python-foundations/module3/main.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Akhil",
+    "age": 25,
+    "skill": "Python"
+}
+
+myintro = {"name":"Akhil-peram","age":23,"skills":"[python,linux,C, go,ML]"}
+print(myintro)
+
+print(student["name"])
