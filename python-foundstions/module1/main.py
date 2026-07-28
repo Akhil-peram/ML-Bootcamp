153a1a5a1ebf9e761d7dd52dac6801daeb750dec
Akhil-peram <akhil01011@gmail.com>
2026-07-28 16:33:15 +0530

    New modules

---
+++ python-foundstions/module1/main.py
@@ -0,0 +1,16 @@
+# dynamically typed language
+
+name = "Akhil" # string
+age = 23 # int
+height = 5.11 # float
+is_working = False # bool
+
+# type annotations like statically typed languages , unnecessary bt very useful and important .
+name2: str = "Tron"
+age2 : int = 90
+height2: float = 6.0
+is_working2 : bool = True
+
+print(name , age , height, is_working)
+
+print(f"{name2} {age2} {height2} {is_working2} ") # fstrings very useful
